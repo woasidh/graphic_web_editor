c5f0fa14e85b3fe8809ae53acbf3a5bc1c369bc4
최민우 <woasidh1@naver.com>
2022-02-01 01:46:31 +0900

    webpack - sass-loader - htmlwebpackplugin - webpack dev-server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import PI from './math'
 import secretKey from './string'
+import './style.scss';
 
 console.log(PI, secretKey, 'hii');
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
   module: {
     rules: [
+      // scss css로 전처리 -> css load
       {
-        test: /\.css$/,
-        use: ['css-loader'],
+        test: /\.scss$/,
+        use: ['css-loader', 'sass-loader'],
         exclude: /node_modules/
       },
+      // ts 파일 load할 수 있게
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -24,4 +27,15 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  // server는 build에 포함 안됨
+  devServer: {
+    port: 80,
+  },
+  // src/index.html에 번들링된 js 파일 끼워넣은 html 파일로 빌드
+  plugins: [
+    new HtmlWebpackPlugin({
+      // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
+      template: 'src/index.html',
+    }),
+  ],
 }
